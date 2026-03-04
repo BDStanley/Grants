4dc32bc1227765a5ca64fc1720d0fdccb60a54fb
BDStanley <bdstanley@gmail.com>
2026-03-04 20:44:39 +0100

    Update 2026-03-04 20:44:39

--- commit.R
+++ commit.R
@@ -3,7 +3,7 @@ system("git add -A")
 system('git commit -m "Update $(date +"%Y-%m-%d %H:%M:%S")"')
 system("git push")
 system(
-  '/opt/homebrew/bin/rsync -av --delete --checksum --exclude=".git" --exclude="Archive" \\
+  '/opt/homebrew/bin/rsync -av --delete --modify-window=1 --exclude=".git" --exclude="Archive" \\
   --protect-args \\
   "/Users/benstanley/Positron/Grants/" \\
   "/Users/benstanley/Library/Mobile Documents/com~apple~CloudDocs/Grants/"'
